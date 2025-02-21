7e3fa6221f3fbbe3a8ab4dd8181612d32b3254b0
RapidOstrich <118029842+RapidOstrich@users.noreply.github.com>
2025-02-20 22:08:13 -0800

    Update cpu.v

--- cpu.v
+++ cpu.v
@@ -37,29 +37,74 @@ module cpu(
     inout [31:0] dmem_data
 );
     
-        
-    wire [11:0] num;
-    
     /*----FLAGS----*/
     reg prog_start;    
     
+    /*----Wire Connections----*/
+    wire imm_sel;
+    wire [31:0] mux_result, rs1_value, rs2_value, alu_result;
+    wire [11:0] imm_value;
+    wire [6:0] funct7, opcode;
+    wire [4:0] rd_sel, rs1_sel, rs2_sel;
+    wire [2:0] funct3;    
+    
     /*----Pipeline Registers----*/
-    reg [31:0] fetch;
+    reg [31:0] instr_fetch;
     
-    /*----Clock Cycle Coutner----*/
-    cc_counter pc(rst_n, clk, /*--TODO--*/, /*--TODO--*/);
     
-    /*----Decoder----*/
-    decoder dc(.instruction(fetch), .imm_value(num) /*OUTPUTS*/);
+    /*----Block Components----*/
+    cc_counter ccc(
+        .rst_n(rst_n),
+        .clk(clk)
+        /*----TODO----*/
+    );
+    
+    decoder dcr(
+        .instruction(instr_fetch),
+        .funct3_out(funct3),
+        .funct7_out(funct7),
+        .rd_sel_out(rd_sel),
+        .rs1_sel_out(rs1_sel),
+        .rs2_sel_out(rs2_sel),
+        .imm_value_out(imm_value),
+        .opcode_out(opcode),
+        .imm_sel_out(imm_sel)
+    );
+    
+    mux_alu mux(
+        .imm_sel_in(imm_sel),
+        .imm_value_in(imm_value),
+        .rs2_in(rs2_value),
+        .mux_out(mux_result)
+    );
+    
+    alu alu(
+        .opcode_in(opcode),
+        .funct3_in(funct3),
+        .funct7_in(funct7),
+        .rs1_in(rs1_value),
+        .mux_result_in(mux_result),
+        .alu_result_out(alu_result)
+    );
+    
+    register_file rgf(
+        .clk(clk),
+        .write_enable(/*----TODO----*/),
+        .rd_sel_in(rd_sel),
+        .rs1_sel_in(rs1_sel),
+        .rs2_sel_in(rs2_sel),
+        .write_data_in(alu_result),
+        .rs1_value_out(rs1_value),
+        .rs2_value_out(rs2_value)
+    );
     
     /*----FETCH----*/
     always @(posedge clk) begin
-        fetch <= imem_insn;
-        //$display("%0b", num);
+        instr_fetch <= imem_insn;
     end
+    
+    
     
     /*----DECODE----*/
   
 endmodule
-
-
