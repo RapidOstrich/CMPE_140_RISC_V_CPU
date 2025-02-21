688a41add045520d6249284a03c21696f6e47d72
RapidOstrich <118029842+RapidOstrich@users.noreply.github.com>
2025-02-21 13:33:34 -0800

    Update alu.v

--- alu.v
+++ alu.v
@@ -23,7 +23,7 @@
 module alu(
     input [6:0] opcode_in, funct7_in,
     input [2:0] funct3_in,
-    input [31:0] rs1_in, mux_result_in,    
+    input [31:0] rs1_value_in, mux_result_in,    
     output reg [31:0] alu_result_out
 );
 
@@ -35,17 +35,17 @@ module alu(
         case (opcode_in)
             immediate: begin
                 case (funct3_in)
-                    3'b000: alu_result_out <= rs1_in + mux_result_in;
+                    3'b000: alu_result_out <= rs1_value_in + mux_result_in;
                   //3'b001: // Shift Left Logical Imm
                   //3'b010: // Set Less Than Imm
                   //3'b011: // Set Less Than Imm (U)
-                    3'b100: alu_result_out <= rs1_in ^ mux_result_in;
+                    3'b100: alu_result_out <= rs1_value_in ^ mux_result_in;
                     3'b101: begin
                         //if (funct7_in[11:5] == 2'h00)
                         //else
                     end
-                    3'b110: alu_result_out <= rs1_in | mux_result_in;
-                    3'b111: alu_result_out <= rs1_in & mux_result_in;          
+                    3'b110: alu_result_out <= rs1_value_in | mux_result_in;
+                    3'b111: alu_result_out <= rs1_value_in & mux_result_in;          
                 endcase
             end
             
