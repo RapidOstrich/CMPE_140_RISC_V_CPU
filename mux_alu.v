1ec92e64bdf39ec17d8a7d4c158457d9d1dd69ad
RapidOstrich <118029842+RapidOstrich@users.noreply.github.com>
2025-02-27 18:36:38 -0800

    Update mux_alu.v

--- mux_alu.v
+++ mux_alu.v
@@ -1,19 +1,19 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: SJSU
+// Engineer: Spenser The
 // 
-// Create Date: 02/20/2025 05:15:28 PM
-// Design Name: 
+// Create Date: 02/20/2025 04:34:50 PM
+// Design Name: RISC-V ALU Input MUX
 // Module Name: mux_alu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
+// Project Name: CMPE 140 RISC-V Processor
+// Target Devices: NA
+// Tool Versions: NA
+// Description: Handles the input selection for the ALU.
 // 
-// Dependencies: 
+// Dependencies: NA
 // 
-// Revision:
+// Revision: 1.0
 // Revision 0.01 - File Created
 // Additional Comments:
 // 
@@ -27,6 +27,7 @@ module mux_alu(
     output [31:0] mux_result_out
 );
 
+    /*----Sign Extenstion Immediate OR rs2----*/
     assign mux_result_out = (imm_sel_in) ? {{20{imm_value_in[11]}}, imm_value_in} : rs2_in;
 
 endmodule
