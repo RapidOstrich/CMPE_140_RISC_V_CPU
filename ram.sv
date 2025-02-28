472a2ce2b1852ae423296c144e8976d7c5b45bdf
RapidOstrich <118029842+RapidOstrich@users.noreply.github.com>
2025-02-27 18:50:41 -0800

    Update ram.sv

--- ram.sv
+++ ram.sv
@@ -9,19 +9,20 @@ module ram #(addr_width = 4, data_width = 4, string init_file = "dummy.dat" )(
     inout [data_width-1:0]data
 );
 
+    /*----Array of 16x4 bit RAM----*/
     reg [data_width-1:0] mem [(1 << addr_width) - 1:0];
     
     initial begin
         $readmemb(init_file, mem);
     end
         
-    assign data = rst_n ? ( wen ? 'z : mem[addr]) : 'z;
+    assign data = (rst_n) ? ( (wen) ? 'z : mem[addr]) : 'z;
     
     always_ff @ (posedge clk) begin
             if (rst_n) begin
-                    if (wen) begin
-                        mem[addr] <= #0.1 data;
-                    end
+                if (wen) begin
+                    mem[addr] <= #0.1 data;
+                end
             end
     end
     
