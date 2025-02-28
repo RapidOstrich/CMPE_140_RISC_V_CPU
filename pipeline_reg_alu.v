310d4e0684aa98760b7eb9bc5e0829fe843719b8
RapidOstrich <118029842+RapidOstrich@users.noreply.github.com>
2025-02-27 18:40:20 -0800

    Updated pipeline_reg_alu.v

--- pipeline_reg_alu.v
+++ pipeline_reg_alu.v
@@ -1,19 +1,19 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: SJSU
-// Engineer: Spenser The
+// Company: 
+// Engineer: 
 // 
-// Create Date: 02/20/2025 04:34:50 PM
-// Design Name: RISC-V Pipeline Register
+// Create Date: 02/21/2025 11:05:43 AM
+// Design Name: 
 // Module Name: pipeline_reg_alu
-// Project Name: CMPE 140 RISC-V Processor
-// Target Devices: NA
-// Tool Versions: NA
-// Description: Pipeline register for execute stage.
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
 // 
-// Dependencies: NA
+// Dependencies: 
 // 
-// Revision: 1.0
+// Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
 // 
@@ -27,12 +27,18 @@ module pipeline_reg_alu(
     input [31:0] alu_result_in,
     output reg write_enable_out,
     output reg [5:0] rd_sel_out,
-    output reg [31:0] alu_result_out
+    output reg [31:0] alu_result_out,
+    
+/*--------Trace Debugging--------*/
+    output reg [31:0] trace_result    
 );
 
     always @(posedge clk) begin
         rd_sel_out <= rd_sel_in;
         alu_result_out <= alu_result_in;
+        
+        /*----Trace Debugging----*/
+        trace_result <= alu_result_in;
     end
 
 endmodule
