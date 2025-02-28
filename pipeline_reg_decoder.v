0f13ce446118a4c698474d90f611c16b13933e77
RapidOstrich <118029842+RapidOstrich@users.noreply.github.com>
2025-02-27 18:40:41 -0800

    Update pipeline_reg_decoder.v

--- pipeline_reg_decoder.v
+++ pipeline_reg_decoder.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 02/21/2025 11:05:43 AM
+// Create Date: 02/21/2025 09:39:06 AM
 // Design Name: 
-// Module Name: pipeline_reg_alu
+// Module Name: pipeline_reg_decoder
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,25 +20,28 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module pipeline_reg_alu(
+module pipeline_reg_decoder(
     input clk,
     input write_enable_in,
-    input [5:0] rd_sel_in,
-    input [31:0] alu_result_in,
+    input [31:0] mux_result_in, rs1_value_in,
+    input [6:0] opcode_in, funct7_in,
+    input [4:0] rd_sel_in,
+    input [2:0] funct3_in,
     output reg write_enable_out,
-    output reg [5:0] rd_sel_out,
-    output reg [31:0] alu_result_out,
-    
-/*--------Trace Debugging--------*/
-    output reg [31:0] trace_result    
+    output reg [31:0] mux_result_out, rs1_value_out,
+    output reg [6:0] opcode_out, funct7_out,
+    output reg [4:0] rd_sel_out,
+    output reg [2:0] funct3_out  
 );
-
+    
     always @(posedge clk) begin
+        write_enable_out <= write_enable_in;
+        mux_result_out <= mux_result_in;
+        rs1_value_out <= rs1_value_in;
+        opcode_out <= opcode_in;
+        funct7_out <= funct7_in;
         rd_sel_out <= rd_sel_in;
-        alu_result_out <= alu_result_in;
-        
-        /*----Trace Debugging----*/
-        trace_result <= alu_result_in;
+        funct3_out <= funct3_in;   
     end
-
+    
 endmodule
