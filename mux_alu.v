4059ec226f52dedf8a54cd30c033d5d88d0bcead
RapidOstrich <118029842+RapidOstrich@users.noreply.github.com>
2025-03-05 21:19:54 -0800

    Start of known good branch

--- mux_alu.v
+++ mux_alu.v
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: SJSU
+// Engineer: Spenser The
 // 
 // Create Date: 02/20/2025 05:15:28 PM
-// Design Name: 
+// Design Name: MUX ALU
 // Module Name: mux_alu
-// Project Name: 
+// Project Name: CMPE 140 RISC-V Processor
 // Target Devices: 
 // Tool Versions: 
-// Description: 
+// Description: 32-bit value input selection for the ALU
 // 
 // Dependencies: 
 // 
@@ -23,14 +23,14 @@
 module mux_alu(
     input imm_sel_in,
     input [11:0] imm_value_in,
-    input [31:0] rs2_in,
+    input [31:0] rs2_value_in,
     output [31:0] mux_result_out,
     
 /*--------Trace Debugging--------*/
     output [11:0] trace_imm    
 );
 
-    assign mux_result_out = (imm_sel_in) ? {{20{imm_value_in[11]}}, imm_value_in} : rs2_in;
+    assign mux_result_out = (imm_sel_in) ? {{20{imm_value_in[11]}}, imm_value_in} : rs2_value_in;
     
     /*--------Trace Debugging--------*/
     assign trace_imm = imm_value_in;
